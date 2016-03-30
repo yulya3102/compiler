4603b4ad7784cd4214f5cd33a25cd2ced04e6d70
yulya3102 <yulya3102@gmail.com>
2016-03-30 17:25:35 +0300

    Parse files

--- main.cpp
+++ main.cpp
@@ -3,10 +3,17 @@
 #include "codegen.h"
 
 #include <iostream>
+#include <fstream>
 
-int main()
+int main(int argc, char ** argv)
 {
+    std::ifstream in;
+    if (argc == 2)
+        in.open(argv[1]);
+
     ast::parser p;
-    ast::Code code = p.parse(std::cin, std::cout);
+    ast::Code code = p.parse(
+                in.is_open() ? in : std::cin,
+                std::cout);
     codegen::generate(code)->dump();
 }

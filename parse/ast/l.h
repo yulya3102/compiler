684cd8c0b33fe6010c9b974d9a3c50a4706824fb
yulya3102 <yulya3102@gmail.com>
2016-04-13 22:29:29 +0300

    Move ast types to its own header

--- parse/ast/l.h
+++ parse/ast/l.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "types.h"
+
 #include <boost/variant.hpp>
 #include <boost/optional.hpp>
 
@@ -9,51 +11,7 @@
 
 namespace ast
 {
-
 class location;
-
-/* Types */
-struct AtomType
-{
-    enum AtomTypeName
-    {
-        BOOL,
-        INT
-    };
-
-    std::shared_ptr<location> loc;
-    AtomTypeName type;
-};
-
-struct Type;
-
-struct PointerType
-{
-    std::shared_ptr<location> loc;
-    std::shared_ptr<Type> type;
-};
-
-struct FuncType
-{
-    std::shared_ptr<location> loc;
-    std::shared_ptr<Type> rettype;
-    std::list<Type> argtypes;
-};
-
-struct Type
-{
-    template <typename T>
-    Type(const T & t)
-        : loc(t.loc)
-        , type(t)
-    {}
-
-    std::shared_ptr<location> loc;
-    boost::variant<AtomType, PointerType, FuncType> type;
-};
-
-Type int_type(std::shared_ptr<location> loc = nullptr);
-Type bool_type(std::shared_ptr<location> loc = nullptr);
 
 /* Declarations */
 
---
+++ parse/ast/types.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <boost/variant.hpp>
+#include <memory>
+#include <list>
+
+namespace ast
+{
+class location;
+
+/* Types */
+struct AtomType
+{
+    enum AtomTypeName
+    {
+        BOOL,
+        INT
+    };
+
+    std::shared_ptr<location> loc;
+    AtomTypeName type;
+};
+
+struct Type;
+
+struct PointerType
+{
+    std::shared_ptr<location> loc;
+    std::shared_ptr<Type> type;
+};
+
+struct FuncType
+{
+    std::shared_ptr<location> loc;
+    std::shared_ptr<Type> rettype;
+    std::list<Type> argtypes;
+};
+
+struct Type
+{
+    template <typename T>
+    Type(const T & t)
+        : loc(t.loc)
+        , type(t)
+    {}
+
+    std::shared_ptr<location> loc;
+    boost::variant<AtomType, PointerType, FuncType> type;
+};
+
+Type int_type(std::shared_ptr<location> loc = nullptr);
+Type bool_type(std::shared_ptr<location> loc = nullptr);
+}

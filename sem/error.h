066972127a407150a587a0050cf3f36aa63f94b9
yulya3102 <yulya3102@gmail.com>
2016-04-14 13:05:14 +0300

    Add semantic error exception

---
+++ sem/error.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <parse/l.h>
+
+#include <stdexcept>
+#include <memory>
+#include <string>
+#include <sstream>
+
+namespace sem
+{
+struct semantic_error : std::runtime_error
+{
+    semantic_error(const std::shared_ptr<ast::location> & loc,
+                   const std::string & what_arg)
+        : std::runtime_error(what_arg)
+        , loc(loc)
+    {}
+
+    const char * what() const noexcept
+    {
+        std::stringstream ss;
+        ss << *loc << ": " << std::runtime_error::what();
+        return ss.str().c_str();
+    }
+
+    std::shared_ptr<ast::location> loc;
+};
+}

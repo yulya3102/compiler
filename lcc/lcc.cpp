3bcf7002bf1d4fbe26724c66f78850603beac6ad
yulya3102 <yulya3102@gmail.com>
2016-04-16 01:58:28 +0300

    Compile executable directly from lib

--- lcc/lcc.cpp
+++ lcc/lcc.cpp
@@ -6,6 +6,7 @@
 #include <sem/l.h>
 
 #include <llvm/IR/Module.h>
+#include <cstdlib>
 
 void lcc::compile_llvm(std::istream & in, llvm::raw_ostream & out, const std::string & module_name)
 {
@@ -15,4 +16,35 @@ void lcc::compile_llvm(std::istream & in, llvm::raw_ostream & out, const std::st
     std::unique_ptr<llvm::Module> module = codegen::generate(code, module_name.c_str());
 
     module->print(out, nullptr);
+}
+
+void lcc::compile_executable(std::istream & in, const std::string & output_name)
+{
+    char ll_filename[255];
+    {
+        strcpy(ll_filename, "lcc_XXXXXX.ll");
+        int tmp_fd = mkstemps(ll_filename, 3);
+        llvm::raw_fd_ostream out(tmp_fd, true);
+        compile_llvm(in, out, output_name);
+    }
+
+    char object_filename[255];
+    {
+        strcpy(object_filename, "lcc_XXXXXX.o");
+        int tmp_fd = mkstemps(object_filename, 2);
+        close(tmp_fd);
+        std::string command("llc -o=");
+        command += object_filename;
+        command += " --filetype=obj ";
+        command += ll_filename;
+        std::system(command.c_str());
+    }
+
+    {
+        std::string command("gcc -o ");
+        command += output_name;
+        command += " ";
+        command += object_filename;
+        std::system(command.c_str());
+    }
 }
--- lcc/lcc.h
+++ lcc/lcc.h
@@ -6,4 +6,5 @@
 namespace lcc
 {
 void compile_llvm(std::istream & in, llvm::raw_ostream & out, const std::string & module_name);
+void compile_executable(std::istream & in, const std::string & output_name);
 }

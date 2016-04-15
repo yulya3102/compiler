bc48291ae8e1a9ab06c8f89baf992fd520a48801
yulya3102 <yulya3102@gmail.com>
2016-04-16 02:52:59 +0300

    Add pointers test

--- testing/main.cpp
+++ testing/main.cpp
@@ -20,6 +20,11 @@ TEST(semantic, assignment_type_mismatch)
     EXPECT_SEMANTIC_ERROR("int main() { _Bool a; a = 1; }");
 }
 
+TEST(implemented, pointers)
+{
+    EXPECT_NO_THROW(try_compile("int main() { int a; a = 0; int * b; b = &a; write(*b); return 0; }"));
+}
+
 int main(int argc, char ** argv)
 {
     testing::InitGoogleTest(&argc, argv);

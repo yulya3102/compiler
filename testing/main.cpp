40e7b144123fc5040b6d55433e5e20e89718b3b7
yulya3102 <yulya3102@gmail.com>
2016-04-15 22:45:58 +0300

    Add some tests

--- testing/main.cpp
+++ testing/main.cpp
@@ -1,4 +1,5 @@
 #include <lcc/lcc.h>
+#include <sem/error.h>
 
 #include <llvm/Support/raw_ostream.h>
 #include <gtest/gtest.h>
@@ -9,6 +10,12 @@ void try_compile(const std::string & code)
     std::stringstream in(code);
     llvm::raw_null_ostream out;
     lcc::compile(in, out, "try_compile");
+}
+
+TEST(semantic, assignment_type_mismatch)
+{
+    EXPECT_THROW(try_compile("int main() { int a; a = true; }"), sem::semantic_error);
+    EXPECT_THROW(try_compile("int main() { _Bool a; a = 1; }"), sem::semantic_error);
 }
 
 int main(int argc, char ** argv)

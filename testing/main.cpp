c30222da02d45209b06696c668d103fb18fb174c
yulya3102 <yulya3102@gmail.com>
2016-04-15 16:37:51 +0300

    Add testing helper

--- testing/main.cpp
+++ testing/main.cpp
@@ -1,4 +1,15 @@
+#include <lcc/lcc.h>
+
+#include <llvm/Support/raw_ostream.h>
 #include <gtest/gtest.h>
+#include <sstream>
+
+void try_compile(const std::string & code)
+{
+    std::stringstream in(code);
+    llvm::raw_null_ostream out;
+    lcc::compile(in, out, "try_compile");
+}
 
 int main(int argc, char ** argv)
 {

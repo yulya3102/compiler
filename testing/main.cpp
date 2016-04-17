df6308ceec5281104cffc2342c177418efc49e4f
yulya3102 <yulya3102@gmail.com>
2016-04-17 18:29:29 +0300

    Add factorial test from examples

--- testing/main.cpp
+++ testing/main.cpp
@@ -1,5 +1,6 @@
 #include <lcc/lcc.h>
 #include <sem/error.h>
+#include <utils/undefined.h>
 
 #include <llvm/Support/raw_ostream.h>
 #include <gtest/gtest.h>
@@ -22,7 +23,47 @@ TEST(semantic, assignment_type_mismatch)
 
 TEST(implemented, pointers)
 {
-    EXPECT_NO_THROW(try_compile("int main() { int a; a = 0; int * b; b = &a; write(*b); return 0; }"));
+    try_compile("int main() { int a; a = 0; int * b; b = &a; write(*b); return 0; }");
+}
+
+std::string test_compiled(const std::string & code, const std::string & input)
+{
+    undefined;
+}
+
+TEST(compiled, fact)
+{
+    std::string code =
+        "int fact(int n);"
+        ""
+        "int main()"
+        "{"
+        "    int n;"
+        "    int res;"
+        "    while (true)"
+        "    {"
+        "        read(n);"
+        "        res = fact(n);"
+        "        write(res);"
+        "    }"
+        "    return 0;"
+        "}"
+        ""
+        "int fact(int n)"
+        "{"
+        "    if (n < 2)"
+        "        return 1;"
+        "    else {}"
+        "    return n * fact(n - 1);"
+        "}";
+    std::stringstream input, expected_output;
+    std::function<int(int)> fact = [&fact] (int n) { if (n > 1) return n * fact(n - 1); else return 1; };
+    for (std::size_t i = 1; i <= 10; ++i)
+    {
+        input << i << std::endl;
+        expected_output << fact(i) << std::endl;
+    }
+    EXPECT_EQ(test_compiled(code, input.str()), expected_output.str());
 }
 
 int main(int argc, char ** argv)

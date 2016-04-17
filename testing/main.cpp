b229992253df4091ec030914f8b17513abd9eabc
yulya3102 <yulya3102@gmail.com>
2016-04-17 18:33:47 +0300

    Add scope test from examples

--- testing/main.cpp
+++ testing/main.cpp
@@ -66,6 +66,48 @@ TEST(compiled, fact)
     EXPECT_EQ(test_compiled(code, input.str()), expected_output.str());
 }
 
+TEST(compiled, scope)
+{
+    std::string code = R"(
+// Test function calls
+
+int ten()
+{
+    return 10;
+}
+
+int a;
+
+int initA(int n)
+{
+    a = n;
+    int b;    b = 14;
+    return 0;
+}
+
+int main()
+{
+    write(ten());       // prints "10"
+    write(a);           // prints "0"
+    int b;
+    b = 88;
+    write(b);           // prints "88"
+    int ret;
+    ret = initA(42);    // function uses 'a' from outer scope and 'b' from its own scope
+    write(a);           // prints "42"
+    write(b);           // prints "88"
+
+    return 0;
+})";
+    std::stringstream expected_output;
+    expected_output << 10 << std::endl;
+    expected_output << 0 << std::endl;
+    expected_output << 88 << std::endl;
+    expected_output << 42 << std::endl;
+    expected_output << 88 << std::endl;
+    EXPECT_EQ(test_compiled(code, ""), expected_output.str());
+}
+
 int main(int argc, char ** argv)
 {
     testing::InitGoogleTest(&argc, argv);

ee8aa72618c992a8b74c8e52db10a1239cfbd97d
yulya3102 <yulya3102@gmail.com>
2016-05-06 01:54:09 +0300

    Generate codegen AST from parse AST

--- gen/ast/l.cpp
+++ gen/ast/l.cpp
@@ -1,8 +1,131 @@
 #include "l.h"
 
 #include <utils/undefined.h>
+#include <utils/fmap.h>
 
-std::list<codegen::CodeEntry> && codegen::construct_entries(const std::list<ast::CodeEntry> & entries)
+namespace codegen
 {
-    undefined;
+
+namespace
+{
+void insert_entry(const ast::Declaration & decl, std::list<CodeEntry> & entries)
+{ }
+
+Variable construct_entry(const ast::VarDeclaration & var)
+{
+    return var;
+}
+
+Function construct_entry(const ast::FuncDefinition & func)
+{
+    return func;
+}
+
+void insert_entry(const ast::Definition & decl, std::list<CodeEntry> & entries)
+{
+    CodeEntry entry = fmap([&decl], x,
+                           CodeEntry(construct_entry(x)),
+                           decl.definition);
+    entries.push_back(entry);
+}
+}
+
+std::list<codegen::CodeEntry> construct_entries(const std::list<ast::CodeEntry> & entries)
+{
+    std::list<codegen::CodeEntry> gen_entries;
+
+    for (auto entry : entries)
+        fmap([&gen_entries], x, insert_entry(x, gen_entries), entry.entry);
+
+    return gen_entries;
+}
+
+namespace
+{
+void insert_variable(const ast::Assignment & st, std::list<Variable> & entries) { }
+void insert_variable(const ast::Write & st, std::list<Variable> & entries) { }
+void insert_variable(const ast::Return & st, std::list<Variable> & entries) { }
+
+void insert_variable(const ast::VarDeclaration & st, std::list<Variable> & entries)
+{
+    entries.push_back(st);
+}
+
+void insert_variable(const ast::Block & st, std::list<Variable> & entries);
+
+void insert_variable(const ast::If & st, std::list<Variable> & entries)
+{
+    insert_variable(*st.thenBody, entries);
+    insert_variable(*st.elseBody, entries);
+}
+
+void insert_variable(const ast::While & st, std::list<Variable> & entries)
+{
+    insert_variable(*st.body, entries);
+}
+
+void insert_variable(const ast::Block & st, std::list<Variable> & entries)
+{
+    for (auto statement : st.statements)
+        fmap([&entries], x, insert_variable(x, entries), statement.statement);
+}
+}
+
+std::list<Variable> function_variables(const ast::FuncDefinition & func)
+{
+    std::list<Variable> variables;
+
+    insert_variable(func.statements, variables);
+
+    return variables;
+}
+
+namespace
+{
+void insert_statements(const ast::VarDeclaration & st, std::list<Statement> & entries) { }
+
+void insert_statements(const ast::Assignment & st, std::list<Statement> & entries)
+{
+    entries.push_back(Statement(st));
+}
+
+void insert_statements(const ast::Write & st, std::list<Statement> & entries)
+{
+    entries.push_back(Statement(st));
+}
+
+void insert_statements(const ast::Return & st, std::list<Statement> & entries)
+{
+    entries.push_back(Statement(st));
+}
+
+void insert_statements(const ast::Block & st, std::list<Statement> & entries);
+
+void insert_statements(const ast::If & st, std::list<Statement> & entries)
+{
+    insert_statements(*st.thenBody, entries);
+    insert_statements(*st.elseBody, entries);
+}
+
+void insert_statements(const ast::While & st, std::list<Statement> & entries)
+{
+    insert_statements(*st.body, entries);
+}
+
+void insert_statements(const ast::Block & st, std::list<Statement> & entries)
+{
+    for (auto statement : st.statements)
+        fmap([&entries], x, insert_statements(x, entries), statement.statement);
+}
+}
+
+std::list<Statement> function_statements(const ast::FuncDefinition & func)
+{
+    std::list<Statement> statements;
+
+    insert_statements(func.statements, statements);
+
+    return statements;
+}
+
 }
--- gen/ast/l.h
+++ gen/ast/l.h
@@ -14,7 +14,8 @@ struct Statement
 {
     template <typename T>
     explicit Statement(const T & t)
-        : statement(t)
+        : loc(t.loc)
+        , statement(t)
     {}
 
     std::shared_ptr<ast::location> loc;
@@ -27,15 +28,22 @@ struct Statement
         > statement;
 };
 
-struct Variable
-{
-    std::shared_ptr<ast::location> loc;
-    ast::Type type;
-    std::string name;
-};
+using Variable = ast::VarDeclaration;
+
+std::list<Variable> function_variables(const ast::FuncDefinition & func);
+std::list<Statement> function_statements(const ast::FuncDefinition & func);
 
 struct Function
 {
+    Function(const ast::FuncDefinition & func)
+        : loc(func.loc)
+        , type(func.declaration.type)
+        , name(func.declaration.name)
+        , arguments(func.declaration.arguments)
+        , variables(function_variables(func))
+        , statements(function_statements(func))
+    {}
+
     std::shared_ptr<ast::location> loc;
     ast::Type type;
     std::string name;
@@ -48,14 +56,15 @@ struct CodeEntry
 {
     template <typename T>
     CodeEntry(const T & t)
-        : entry(t)
+        : loc(t.loc)
+        , entry(t)
     {}
 
     std::shared_ptr<ast::location> loc;
     boost::variant<Variable, Function> entry;
 };
 
-std::list<CodeEntry> && construct_entries(const std::list<ast::CodeEntry> & entries);
+std::list<CodeEntry> construct_entries(const std::list<ast::CodeEntry> & entries);
 
 struct Code
 {

3ade693e62968fd57d0fc726cbdb2989283ad238
Pyankova Julia <yulya3102@gmail.com>
2016-05-06 18:09:43 +0300

    Enable TCO

--- lcc/lcc.cpp
+++ lcc/lcc.cpp
@@ -15,7 +15,7 @@ void lcc::compile_llvm(std::istream & in, llvm::raw_ostream & out, const std::st
     ast::parser p;
     ast::Code code = p.parse(in, std::cout);
     sem::verify(code);
-    //optimise::optimise_tail_call(code);
+    optimise::optimise_tail_call(code);
     codegen::Code gen_code(code);
     std::unique_ptr<llvm::Module> module = codegen::generate(gen_code, module_name.c_str());
 

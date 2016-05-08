6ebb8d9928fa316094d51773d2617ce6105252b0
yulya3102 <yulya3102@gmail.com>
2016-05-09 02:03:09 +0300

    Implementations

--- optimise/accum.cpp
+++ optimise/accum.cpp
@@ -6,12 +6,12 @@ namespace optimise
 {
 namespace
 {
-std::list<ast::Value> get_non_recursive_returns(const codegen::Function & f)
+std::list<ast::Expression> get_non_recursive_returns(const codegen::Function & f)
 {
     undefined;
 }
 
-std::list<ast::Value> get_recursive_returns(const codegen::Function & f)
+std::list<ast::Expression> get_recursive_returns(const codegen::Function & f)
 {
     undefined;
 }
@@ -23,25 +23,20 @@ void rewrite_returns_to_acc(codegen::Function & f)
 
 std::list<codegen::Function> optimise(codegen::Function & f)
 {
-    std::list<ast::Value> non_recursive = get_non_recursive_returns(f);
-    std::list<ast::Value> recursive = get_recursive_returns(f);
+    std::list<ast::Expression> non_recursive = get_non_recursive_returns(f);
+    std::list<ast::Expression> recursive = get_recursive_returns(f);
 
     if (non_recursive.size() != 1)
         return {};
     if (recursive.size() != 1)
         return {};
 
-    ast::Value init_acc = non_recursive.front();
+    ast::Expression init_acc = non_recursive.front();
     codegen::Function func_acc(f);
     func_acc.arguments.push_back({f.loc, f.type, "_accumulator"});
     f.statements.clear();
     f.statements.push_back(
-        codegen::Statement(
-            ast::Return{
-                f.loc,
-                undefined_expr(ast::Expression)
-            }
-        )
+        codegen::Statement(ast::Return{f.loc, init_acc})
     );
     rewrite_returns_to_acc(func_acc);
     return { func_acc };

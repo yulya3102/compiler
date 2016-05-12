8e49c0d0c4dc802ffbd2ea7113b4044782da0eb8
yulya3102 <yulya3102@gmail.com>
2016-05-12 19:44:33 +0300

    Use optimisation level from cmdline

--- lcc/main.cpp
+++ lcc/main.cpp
@@ -6,16 +6,49 @@
 #include <iostream>
 #include <fstream>
 
+void usage(const char * program)
+{
+    std::cerr << "Usage: " << program << " [-O{0,1,2}] <input> <output>" << std::endl;
+    exit(EXIT_FAILURE);
+}
+
 int main(int argc, char ** argv)
 {
-    if (argc != 3)
+    if (argc < 3)
+        usage(argv[0]);
+
+    lcc::Optimisations opt = lcc::Optimisations::ACC;
+    const char * input = argv[1];
+    const char * output = argv[2];
+
+    if (argv[1][0] == '-')
     {
-        std::cerr << "Usage: " << argv[0] << " <input> <output>" << std::endl;
-        return EXIT_FAILURE;
+        if (argv[1][1] != 'O')
+            usage(argv[0]);
+        switch (argv[1][2])
+        {
+            case '0':
+                opt = lcc::Optimisations::NONE;
+                break;
+            case '1':
+                opt = lcc::Optimisations::TCO;
+                break;
+            case '2':
+                opt = lcc::Optimisations::TCO;
+                break;
+            default:
+                usage(argv[0]);
+        }
+
+        if (argc != 4)
+            usage(argv[0]);
+
+        input = argv[2];
+        output = argv[3];
     }
 
-    std::ifstream in(argv[1]);
+    std::ifstream in(input);
     std::error_code err;
-    llvm::raw_fd_ostream out(argv[2], err, llvm::sys::fs::OpenFlags::F_None);
-    lcc::compile_llvm(in, out, argv[1]);
+    llvm::raw_fd_ostream out(output, err, llvm::sys::fs::OpenFlags::F_None);
+    lcc::compile_llvm(in, out, input, opt);
 }
